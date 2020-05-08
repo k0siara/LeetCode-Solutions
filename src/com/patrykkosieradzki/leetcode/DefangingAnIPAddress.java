3a3576ab31075f4ca1560357228788a8d994ce7c
k0siara <patryk.kosieradzki@gmail.com>
2020-05-09 00:12:47 +0200

    Solved DefangingAnIPAddress

---
+++ src/com/patrykkosieradzki/leetcode/DefangingAnIPAddress.java
@@ -0,0 +1,31 @@
+package com.patrykkosieradzki.leetcode;
+
+/**
+ * 1108. Defanging an IP Address
+ *
+ * Given a valid (IPv4) IP address, return a defanged version of that IP address.
+ *
+ * A defanged IP address replaces every period "." with "[.]".
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: address = "1.1.1.1"
+ * Output: "1[.]1[.]1[.]1"
+ * Example 2:
+ *
+ * Input: address = "255.100.50.0"
+ * Output: "255[.]100[.]50[.]0"
+ */
+
+public class DefangingAnIPAddress {
+
+    public static void main(String[] args) {
+        System.out.println(defangIPaddr("1.1.1.1"));
+    }
+
+    public static String defangIPaddr(String address) {
+        return address.replaceAll("\\.", "[.]");
+    }
+}
